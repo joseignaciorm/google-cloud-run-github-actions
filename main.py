7fbd84f2221a32e19dd0c6aa626b6538a101cb70
Nacho <joseignaciorm13@gmail.com>
2020-11-22 10:24:11 +0100

    Solve indent problen in code

--- main.py
+++ main.py
@@ -20,35 +20,34 @@ def hello_world():
 
     # 1. Debug token: input access token, display token debug details
     if st.checkbox("Debug token"):
-    st.subheader("Check the debug token")
-    access_token = st.text_input("Enter debug token")
+        st.subheader("Check the debug token")
+        access_token = st.text_input("Enter debug token")
 
         if st.button("Submit"):
-        result = access_token.title()
-        response = debug_access_token(access_token)
-        data_access_expires_at = datetime.fromtimestamp(response['data']['data_access_expires_at'])
+            result = access_token.title()
+            response = debug_access_token(access_token)
+            data_access_expires_at = datetime.fromtimestamp(
+                response["data"]["data_access_expires_at"]
+            )
 
-        st.subheader("Data result:")
-        st.write("Data access expires at:", data_access_expires_at)
-        
-        st.write(response)
+            st.subheader("Data result:")
+            st.write("Data access expires at:", data_access_expires_at)
 
-        
-            
+            st.write(response)
 
-    #2. Exchange initial token: inputs client_id, client_secret, access_token and debug, display new token and debug
+    # 2. Exchange initial token: inputs client_id, client_secret, access_token and debug, display new token and debug
     if st.checkbox("Exchange initial token"):
-    st.subheader("Exchange initial token")
+        st.subheader("Exchange initial token")
 
-    client_id = st.text_input("Client ID")
-    client_secret = st.text_input("Client secret")
-    access_t = st.text_input("Insert your access token")
+        client_id = st.text_input("Client ID")
+        client_secret = st.text_input("Client secret")
+        access_t = st.text_input("Insert your access token")
 
         if st.button("Send to exchange"):
 
-        response = exchange_token(client_id, client_secret, access_t)
-        st.subheader("Data result:")
-        st.write(response)
+            response = exchange_token(client_id, client_secret, access_t)
+            st.subheader("Data result:")
+            st.write(response)
 
 
 if __name__ == "__main__":
